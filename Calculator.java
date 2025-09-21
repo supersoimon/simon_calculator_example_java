98d5301935c4fbf714cf38f0765152f0135bdc25
supersoimon <simon.gidrewicz@gmail.com>
2025-09-21 14:43:23 -0600

    six seven

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,7 @@
 package simon_calculator_example_java;
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("67676767676767676767676767");
+        //six seven six seven gyatt rizz
+        System.out.println("the realest 67676767676767676767676767");
     }
 }
