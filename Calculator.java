facf0d8467a3b50d75088a219d071f22439ce4fc
supersoimon <simon.gidrewicz@gmail.com>
2025-09-28 12:11:16 -0600

    six sevenss

--- Calculator.java
+++ Calculator.java
@@ -9,9 +9,19 @@ public class Calculator {
         Scanner scanner;
         try {
             scanner = new Scanner(new File("input.txt"));
+            while (scanner.hasNextLine()) {
+                String inputLine = scanner.nextLine();
+                operation = inputLine.charAt(0);
+                num1 = Double.parseDouble(inputLine.substring(inputLine.indexOf(" "),
+                                          inputLine.lastIndexOf(" ")));
+                num2 = Double.parseDouble(inputLine.substring(inputLine.indexOf(" "),
+                                          inputLine.lastIndexOf(" ")));
+                System.out.printf("num1=%.2f, num2=%.2f, operation=%s\n", num1, num2, operation);
+            }  
         } catch (FileNotFoundException e) {
             System.err.println("File not found!");
         }
+        
 
     }
 }
